027abedac13de3aab9ebe77c15529dd80e4a0e36
Fatur Gautama S <fatur@access-mobile.com>
2023-11-05 14:39:39 +0700

    UPDATE

--- src/app/eduman/common/header/headertwo/headertwo.component.ts
+++ src/app/eduman/common/header/headertwo/headertwo.component.ts
@@ -1,61 +1,74 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { HostListener } from '@angular/core';
+import { CoreService } from 'src/app/services/core.service';
 
 @Component({
-  selector: 'app-headertwo',
-  templateUrl: './headertwo.component.html',
-  styleUrls: ['./headertwo.component.scss'],
-  encapsulation: ViewEncapsulation.None,
+    selector: 'app-headertwo',
+    templateUrl: './headertwo.component.html',
+    styleUrls: ['./headertwo.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class HeadertwoComponent implements OnInit {
-  //cart sidebar activation start
-  sidebarCartActive: boolean = false;
-  cartclick() {
-    if (this.sidebarCartActive == false) {
-      this.sidebarCartActive = true;
-      this.sidebarInfoActive = false;
-    } else {
-      this.sidebarCartActive = false;
+    //cart sidebar activation start
+    sidebarCartActive: boolean = false;
+    cartclick() {
+        if (this.sidebarCartActive == false) {
+            this.sidebarCartActive = true;
+            this.sidebarInfoActive = false;
+        } else {
+            this.sidebarCartActive = false;
+        }
     }
-  }
-  //cart sidebar activation end
+    //cart sidebar activation end
 
-  //sidebar info click activation start
-  sidebarInfoActive: boolean = false;
-  infoclick() {
-    if (this.sidebarInfoActive == false) {
-      this.sidebarCartActive = false;
-      this.sidebarInfoActive = true;
-    } else {
-      this.sidebarInfoActive = false;
+    //sidebar info click activation start
+    sidebarInfoActive: boolean = false;
+    infoclick() {
+        if (this.sidebarInfoActive == false) {
+            this.sidebarCartActive = false;
+            this.sidebarInfoActive = true;
+        } else {
+            this.sidebarInfoActive = false;
+        }
     }
-  }
-  //sidebar info click activation end
+    //sidebar info click activation end
 
-  //counter
-  counters: { [key: string]: number } = {
-    count: 1,
-    count1: 1,
-    count2: 1,
-  };
+    //counter
+    counters: { [key: string]: number } = {
+        count: 1,
+        count1: 1,
+        count2: 1,
+    };
 
-  counter(key: string, type: string) {
-    if (type === 'add') {
-      this.counters[key]++;
-    } else if (type === 'subtract' && this.counters[key] > 1) {
-      this.counters[key]--;
+    counter(key: string, type: string) {
+        if (type === 'add') {
+            this.counters[key]++;
+        } else if (type === 'subtract' && this.counters[key] > 1) {
+            this.counters[key]--;
+        }
     }
-  }
 
-  //sticky header activation
-  isSticky: boolean = false;
+    //sticky header activation
+    isSticky: boolean = false;
 
-  @HostListener('window:scroll', ['$event'])
-  checkScroll() {
-    this.isSticky = window.pageYOffset >= 40;
-  }
-  //sticky header activation
-  constructor() {}
+    @HostListener('window:scroll', ['$event'])
+    checkScroll() {
+        this.isSticky = window.pageYOffset >= 40;
+    }
+    //sticky header activation
+    Identitas: any;
 
-  ngOnInit(): void {}
+    constructor(
+        private _coreService: CoreService
+    ) { }
+
+    ngOnInit(): void {
+        this._coreService.getIdentitas().subscribe((result) => {
+            if (result.status) {
+                result.data.instansi = result.data.instansi.replace(" ", "<br>")
+
+                this.Identitas = result.data;
+            }
+        })
+    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    endpoint: 'http://apiproduction.universitassimalungun.ac.id/portal_dosen',
+    endpoint: 'https://apiproduction.universitassimalungun.ac.id/portal_dosen',
     token: 'b33c6605-e2e8-40f0-89d3-b8ca44167c14'
 };
 

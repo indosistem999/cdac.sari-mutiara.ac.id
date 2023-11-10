0babb17c0f49f1a490fa6fd8e2c393e32eed546c
Fatur Gautama S <fatur@access-mobile.com>
2023-11-10 22:12:39 +0700

    UPDATE

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    endpoint: 'https://apiproduction.universitassimalungun.ac.id/website',
+    endpoint: 'https://apiproduction.sari-mutiara.ac.id/website',
     token: 'b33c6605-e2e8-40f0-89d3-b8ca44167c14'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    endpoint: 'https://apiproduction.universitassimalungun.ac.id/website',
+    endpoint: 'https://apiproduction.sari-mutiara.ac.id/website',
     token: 'b33c6605-e2e8-40f0-89d3-b8ca44167c14'
 };
 
